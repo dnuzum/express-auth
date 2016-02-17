5fae665e3f0d0477e6d8afbec4400dc2d161e16d
Derek Nuzum <derek.nuzum@gmail.com>
2016-02-17 14:38:42 -0800

    added password hashing

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+var bcrypt = require('bcrypt');
+
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define('user', {
     name: DataTypes.STRING,
@@ -8,6 +10,19 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+      }
+    },
+    hooks: {
+      beforeCreate: function(user, options, callback) {
+        if (user.password) {
+          bcrypt.hash(user.password, 10, function(err, hash) {
+            if (err) return callback(err);
+            user.password = hash;
+            callback(null, user);
+          });
+        } else {
+          callback(null, user);
+        }
       }
     }
   });
